f516971e0ff060c67d2848ab3b5e4890f7fcdac1
Giulio Canti <giulio.canti@gmail.com>
2017-08-10 14:10:40 +0200

    add ChainRec and Alternative instances (#1)

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+# Features
+
+```ts
+import { fluture } from 'fp-ts-fluture'
+```
+
+- `Monad` instance
+- `Bifunctor` instance
+- `ChainRec` instance
+
+```ts
+import { concurrentFluture } from 'fp-ts-fluture'
+```
+
+- `Alternative` instance
+
 # Example
 
 ```ts
@@ -9,3 +25,4 @@ import * as array from 'fp-ts/lib/Array'
 sequence(fluture, array)([of(1), reject('ops')]).fork(() => console.error('error'), xs => console.log(xs)) // => "error"
 sequence(fluture, array)([of(1), of(2)]).fork(() => console.error('error'), xs => console.log(xs)) // => [1, 2]
 ```
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import { Future, map, of, ap, chain, bimap } from 'fluture'
+import { Next, Done, Future, Par, ConcurrentFuture } from 'fluture'
 import { Monad } from 'fp-ts/lib/Monad'
 import { Bifunctor } from 'fp-ts/lib/Bifunctor'
+import { Alternative } from 'fp-ts/lib/Alternative'
+import { ChainRec } from 'fp-ts/lib/ChainRec'
+import { Either } from 'fp-ts/lib/Either'
+
+//
+// Future
+//
 
 export const URI = 'Fluture'
 
@@ -20,11 +27,47 @@ declare module 'fluture' {
   }
 }
 
-export const fluture: Monad<URI> & Bifunctor<URI> = {
+export function chainRec<L, A, B>(f: (a: A) => Future<L, Either<A, B>>, a: A): Future<L, B> {
+  return Future.chainRec<L, A, B>((next, done, value) => f(value).map(e => e.fold<Next<A> | Done<B>>(next, done)), a)
+}
+
+export const fluture: Monad<URI> & Bifunctor<URI> & ChainRec<URI> = {
   URI,
-  map,
-  of,
-  ap,
-  chain,
-  bimap
+  map: Future.map,
+  of: Future.of,
+  ap: Future.ap,
+  chain: Future.chain,
+  bimap: Future.bimap,
+  chainRec
+}
+
+//
+// ConcurrentFuture
+//
+
+declare module 'fp-ts/lib/HKT' {
+  interface URI2HKT2<L, A> {
+    ConcurrentFluture: ConcurrentFuture<L, A>
+  }
+}
+
+declare module 'fluture' {
+  interface ConcurrentFuture<L, R> {
+    _A: R
+    _L: L
+    _URI: ConcurrentURI
+  }
+}
+
+export const ConcurrentURI = 'ConcurrentFluture'
+
+export type ConcurrentURI = typeof ConcurrentURI
+
+export const concurrentFluture: Alternative<ConcurrentURI> = {
+  URI: ConcurrentURI,
+  map: Par.map,
+  of: Par.of,
+  ap: Par.ap,
+  alt: Par.alt,
+  zero: Par.zero
 }
